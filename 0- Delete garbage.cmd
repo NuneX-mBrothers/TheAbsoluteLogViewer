38d49ea3db8f0f7b8817bacdaea2bdca6b78b3b9
NuneX-mBrothers <nunommendonca@gmail.com>
2026-04-23 15:08:49 +0100

    Release v1.2.92.0

---
+++ 0- Delete garbage.cmd
@@ -0,0 +1,14 @@
+@echo off
+
+echo A Apagar a anterior distribuição "C:\Users\nmend\OneDrive\My Code\LogViewer\LogViewer\bin\Release\net10.0-windows\win-x64"
+rmdir /s /q "C:\Users\nmend\OneDrive\My Code\LogViewer\LogViewer\bin\Release\net10.0-windows\win-x64"
+
+echo A Apagar obj "C:\Users\nmend\OneDrive\My Code\LogViewer\LogViewer\obj"
+rmdir /s /q "C:\Users\nmend\OneDrive\My Code\LogViewer\LogViewer\obj"
+
+echo.
+echo ==========================================
+echo   Lixo apagado
+echo ==========================================
+echo.
+pause
